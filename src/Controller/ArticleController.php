32ae610f4c018ef71dcf1a0138534ac1223f7f53
= <=>
2020-07-08 08:56:57 +0200

    liste des articles et article liés à la bdd

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/article", name="article")
+     * @Route("/article/{id}", name="article")
+     * @param Article $article
+     * @return Response
      */
-    public function index()
+    public function index(Article $article)
     {
         return $this->render('article/article.html.twig', [
-
+            "article" => $article
         ]);
     }
 }
